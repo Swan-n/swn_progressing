69bbfd14305f81cc8406b4dc92c429886361233e
Swan <116465095+Swan-n@users.noreply.github.com>
2022-12-14 21:15:10 +0000

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+By "Swan#2366"
+ui_page 'progressBar.html'
+
+client_scripts {
+    'client.lua'
+} 
+
+files {
+    'progressBar.html'
+}
+
+export "startFunctions"
---
+++ client.lua
@@ -0,0 +1,8 @@
+function startFunctions(time, text) 
+	SendNUIMessage({
+		type = "ui",
+		display = true,
+		time = time,
+		text = text
+	})
+end
